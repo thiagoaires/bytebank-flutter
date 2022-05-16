1290f441f6e45a1b6120a4a87f168af581119f7d
Thiago Aires da Silva <thiago.aires@b2wdigital.com>
2022-05-16 11:02:22 -0300

    aula 01

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -19,42 +19,90 @@ class Dashboard extends StatelessWidget {
                 "images/bytebank_logo.png",
               ),
             ),
-            Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ContactsList();
-                    },
-                  ),
+            Row(
+              children: [
+                _FeatureItem(
+                  name: "Transfer",
+                  icon: Icons.monetization_on,
+                  onClick: () => _showContactList(),
+                  // onClick: () => Navigator.of(context).push(
+                  //   MaterialPageRoute(
+                  //     builder: (context) {
+                  //       return ContactsList();
+                  //     },
+                  //   ),
+                  // ),
                 ),
-                child: Container(
-                  height: 100,
-                  width: 150,
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 32,
-                      ),
-                      Text(
-                        "Contacts",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
+                _FeatureItem(
+                  name: "Transaction Feed",
+                  icon: Icons.description,
+                  onClick: () => debugPrint('bacon'),
+                  // onClick: () => Navigator.of(context).push(
+                  //   MaterialPageRoute(
+                  //     builder: (context) {
+                  //       return ContactsList();
+                  //     },
+                  //   ),
+                  // ),
                 ),
-              ),
+              ],
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  void _showContactList() {
+    debugPrint('ads');
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final IconData icon;
+  final String name;
+  final Function onClick;
+
+  const _FeatureItem({this.icon, this.name = "", @required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: this.onClick,
+          // onTap: () => Navigator.of(context).push(
+          //   MaterialPageRoute(
+          //     builder: (context) {
+          //       return ContactsList();
+          //     },
+          //   ),
+          // ),
+          child: Container(
+            height: 100,
+            width: 150,
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  this.icon ?? Icons.people,
+                  color: Colors.white,
+                  size: 32,
+                ),
+                Text(
+                  this.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
